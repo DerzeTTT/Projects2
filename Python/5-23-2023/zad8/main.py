1361bcee513125189c24d551998eea9cb084eba3
DerzeTTTT <u13_davdro_lbn@technischools.com>
2023-05-23 12:18:26 +0200

    zadania

--- Python/5-23-2023/zad8/main.py
+++ Python/5-23-2023/zad8/main.py
@@ -5,7 +5,7 @@ file1, file2 = open('file1.txt', 'r').read(), open('file2.txt', 'r').read()
 
 def getDifference(str1, str2):
 
-    larger, smaller = None, None;
+    larger, smaller = None, None
 
     if len(str1) >= len(str2):
 
@@ -17,20 +17,42 @@ def getDifference(str1, str2):
         larger = str2
         smaller = str1
 
-    listed1, listed2 = list(larger), list(smaller)
     totalChars = len(larger)
 
+    listed1, listed2 = list(larger), list(smaller)
+
     correct = 0
+    difIndexes = {}
 
-    for i in range(0, len(listed2)):
+    count = 0
 
-        if listed2[i] == listed1[i]:
+    for i in range(0, totalChars):
 
-            correct += 1
+        try:
+
+            if listed2[i] == listed1[i]:
+
+                correct += 1
+
+            else:
+
+                difIndexes[i] = True
+
+        except:
+
+            difIndexes[i] = True
+
+    difStr = ''
+
+    for i in range(len(larger)):
+
+        appending = ((difIndexes.get(i) and f'({listed1[i]})') or listed1[i])
+
+        difStr += appending
 
     percentage = (correct / totalChars) * 100
 
-    return percentage
+    return (percentage, difStr)
 
 if file1 == file2:
 
@@ -38,4 +60,6 @@ if file1 == file2:
 
 else:
 
-    print("Difference in between file content (%):", getDifference(file1, file2), sep="\n")
+    diff = getDifference(file1, file2)
+
+    print("Difference in between file content (%):", diff[0], "Difference in text:", diff[1], sep="\n")
